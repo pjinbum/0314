92923cbf08aad08433bdf54eafcb512df8794312
Insight <119594529+pjinbum@users.noreply.github.com>
2023-03-14 11:44:34 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Header from './filelist/Header';
+import Main from './filelist/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <Header></Header>
+        <Header></Header>
+      </div>
+      <div>
+        <Main></Main>
+        <Main></Main>
+        <Main></Main>
+        <Main></Main>
+       
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
